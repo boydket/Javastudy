8cd46ecb5e12c18799c61204ba474b9fea90163b
boydket <boydket@naver.com>
2024-05-31 20:53:43 +0900

    newexamtest(3gyoshi-opening)

--- src/test/java/com/example/demo/newexamTest.java
+++ src/test/java/com/example/demo/newexamTest.java
@@ -2,25 +2,29 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class newexamTest {
     @Test
     public void subtract001() throws Exception{
-        // given    테스트하기 위한 준비 동작
-        int num1=2;
-        int num2=3;
-        int expected=-1;
+        //Exception 예외가 발생해야지만 정상 동작으로 인식한다.
+       assertThatThrownBy(() -> subTest001(70000, 7))
+               .isInstanceOf(Exception.class);
+       //RuntimeException 예외가 발생해야지만 정상 동작으로 인식한다.
+        Throwable exception = assertThrows(RuntimeException.class, () -> {
+            subTest001(2330, 50002);
+        });
+        System.out.println(exception.toString());
 
-        // when  // 테스트를 실시
-        int actual=subTest001(num1,num2);
-
-        // then  // 실제값과 기대값을 비교해서 테스트한다.
-        assertThat(actual).isEqualTo(expected);
     }
 
     int subTest001(int n1, int n2) throws Exception{
         if(n1<-50000 || n1 > 50000) {
             throw new Exception("num1 값은 -50000보다 작거나, num1 값은 50000 보다 큽니다...");
+        }
+        if ( n2 < -50000 || n2 > 50000) {
+            throw new RuntimeException("num2 값은 -50000보다 작거나, num2 값은 50000보다 큽니다.");
         }
         return n1-n2;
     }
